3bd0e88dbf1034b933185fdefba4349af58e8449
ricffb <35438508+ricffb@users.noreply.github.com>
2020-05-15 09:57:34 +0200

    create feature

---
+++ feature.py
@@ -0,0 +1,18 @@
+import os, sys
+import tensorflow as tf
+import numpy as np
+import random
+
+MSG = [
+    "Hello",
+    "Woots up",
+    "Greetngs,"
+]
+
+
+def better_printer(name):
+    print(MSG[random.randint(0, len(MSG)-1)] + ",", name + "!")
+
+
+if __name__ == '__main__':
+    better_printer(sys.argv[1])
