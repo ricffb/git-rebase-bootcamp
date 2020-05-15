5576afaa63e6e44384bac9d3b3f5fededeb5d84a
ricffb <35438508+ricffb@users.noreply.github.com>
2020-05-15 10:28:42 +0200

    support multiple arguments

--- awesome_file.py
+++ awesome_file.py
@@ -6,5 +6,11 @@ def print_hello(msg):
     print("Hello,", msg, "!")
 
 
+def print_hello_to_everyone(msgs):
+
+    for name in msgs:
+        print("Hello,", name, "!")
+
+
 if __name__ == "__main__":
-    print_hello(sys.argv[1])
+    print_hello_to_everyone(sys.argv[1:])
